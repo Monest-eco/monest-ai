72db7e1b3a42a5897e9967b0cdb4afc33cac016c
Cyril Grosjean <cyril.grosjean@epitech.eu>
2021-12-10 16:20:28 +0100

    feat() : Added Dataset

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 from ai.model import *
+from ai.file import *
+from ai.dataset import *
 
 if __name__ == "__main__":
-    print("Project successfully compiled !")
-    mod = Model.Model()
+    file = File.File("./bin")
+    model = None
+    dataset = None
+
+    if file.get_number_files() > 0:
+        model = Model.Model(file.get_file_path(0), True, file.get_file_name(0), file.get_file_path(0))
+    else:
+        model = Model.Model(save=True, name="ai_result", path="./bin")
+    # data = Dataset.Dataset("./bin/data_fridge.json")
+    # metadata = data.get_shape_values(300)
